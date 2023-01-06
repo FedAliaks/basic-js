f85c3cdfdc3da05320cfa726b0a0a616aef04519
Aliaksandr Fedukovich <1632637@mail.ru>
2023-01-06 19:02:47 +0300

    feat: add solution for 2 tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
+function countCats(matrix) {
+  let count = 0;
+  matrix.forEach(item => {
+    item.forEach(itemInner => itemInner === '^^' ? count ++ : 0)
+  });
+
+  return count;
+
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -13,6 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function sortByHeight(/* arr */) {
   throw new NotImplementedError('Not implemented');
+  console.log('arr: ' + arr)
+  let answer = arr.sort((a, b) => {
+    if(a != -1 && b!= -1) return a - b
+
+  })
+
+  console.log("answer: " + answer);
+  return answer;
+
   // remove line with error and write your code here
 }
 
