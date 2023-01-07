89b82c68ab4c4f760bf14fc1820831cf546e5513
Aliaksandr Fedukovich <1632637@mail.ru>
2023-01-07 09:23:42 +0300

    feat: add solution src/transform-array.js part 1

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
+function transform(arr) {
+  if(!Array.isArray(arr)) throw Error("\'arr\\");
+
+  let answer = [];
+  const indexMax = arr.length - 1;
+  let discardNext = false;
+
+  arr.forEach((item, index) => {
+    console.log('item: ' + item + ' index: ' + index)
+    if(discardNext) {
+      discardNext = false;
+    } else if(item === '--double-next') {
+      if(index !== indexMax) answer.push(arr[index + 1]);
+    } else if(item === '--discard-prev') {
+      if(index !== 0) answer.pop();
+    } else if(item === '--double-prev') {
+      if(index !== 0) answer.push(arr[index - 1]);
+    } else if (item === '--discard-next') {
+      discardNext = true;
+    } else {
+      answer.push(item);
+    }
+  })
+
+  return answer;
+
+
+
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
