43ce25bcd2385614c717e033096c0ce4d4870266
Aliaksandr Fedukovich <1632637@mail.ru>
2023-01-07 15:23:30 +0300

    feat: add extended-repeater. part1

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
+function repeater(str, options) {
+  let temp = str;
+
+  console.log(options.addition)
+
+  optionsSeparator = (options.separator) ? options.separator : "+";
+  optionsAdditionSeparator = (options.additionSeparator) ? options.additionSeparator : "|";
+  optionsAddition = (options.addition) ? options.addition  : "";
+
+
+
+  for(let i = 0; i < options.additionRepeatTimes - 1; i++) {
+    temp += optionsAddition + optionsAdditionSeparator;
+    console.log(temp);
+  }
+
+  temp += optionsAddition;
+
+
+  let answer = '';
+  for(let i = 0; i < options.repeatTimes - 1; i++) {
+    answer += temp + optionsSeparator;
+  }
+
+
+  return answer + temp;
+
+
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
