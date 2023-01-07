142ad46b100610899675fed793b4198156857059
Aliaksandr Fedukovich <1632637@mail.ru>
2023-01-07 14:42:17 +0300

    feat: add solution for dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,8 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if(!Array.isArray(members)) return false;
+  let arrAnswer = [];
+  members.forEach(item => {
+    if(typeof item === 'string') {
+      let temp = item.trim();
+      arrAnswer.push(temp[0].toUpperCase());
+    }
+  });
+
+  let answer = arrAnswer.sort().join('');
+
+  return answer;
+
+
+
+
+
+/*   throw new NotImplementedError('Not implemented'); */
   // remove line with error and write your code here
 }
 
