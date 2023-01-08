e235ee7981dc3cad95fe9606ad7e374403e638c8
Aliaksandr Fedukovich <1632637@mail.ru>
2023-01-08 11:31:57 +0300

    feat: add solution for hanoi tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
+function calculateHanoi(disksNumber, turnsSpeed ) {
+  const objAnswer = {};
+  objAnswer.turns = Math.pow(2, disksNumber) - 1;
+  objAnswer.seconds = Math.floor(objAnswer.turns * 3600 / turnsSpeed);
+  return objAnswer;
+
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
